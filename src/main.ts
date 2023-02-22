bcb771800793be3404178a92171d886b068b489f
AndromedaSmart <asorokin2002@gmail.com>
2023-02-22 18:58:51 +0300

    Second commit (add updated main.ts), added front side of project

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
-import * as dotenv from "dotenv";
-async function bootstrap() {
-  dotenv.config()
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
 
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(
+      AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
